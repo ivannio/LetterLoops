c7c94105e283a7b820766d1a9609c5c434f56914
Ivan Phelps <ivanniothemannio@gmail.com>
2020-03-07 13:31:28 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey bud. Go ahead and type something in and press enter. I'm gonna take it and do some stuff with it and spit it back out at ya.");
+
+            var input = Console.ReadLine();
+
+            var first = char.ToUpper(input[0]) + "-";
+
+            var second = "";
+
+            var concat = "";            
+
+            for (var i = 1; i < input.Length; i++) 
+            {
+                var section = new String(char.ToLower(input[i]), i + 1) + "-";
+                var upperCasedFirst = char.ToUpper(section[0]) + section.Substring(1);
+                concat += upperCasedFirst;                
+            }
+            second = concat.Remove(concat.Length - 1, 1);
+            Console.WriteLine(first + second);
+            Console.ReadLine();
+        }
+    }
+}
